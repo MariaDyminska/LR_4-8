f867d56c6161f5cbccd1233ed1334ea7eede9df0
Maria Dyminska <mariia.dyminska.oi.2024@lpnu.ua>
2025-11-30 02:10:23 +0200

    логування

--- src/test/java/menu/EarlyPayoffCommandTest.java
+++ src/test/java/menu/EarlyPayoffCommandTest.java
@@ -11,29 +11,27 @@ public class EarlyPayoffCommandTest {
     @Test
     public void testEarlyPayoffCommand_ClosesCreditButDoesNotRemove() {
 
-        // підготовка системи
+
         CreditSystem system = new CreditSystem();
         system.addCredit(new ConsumerCredit(1, "Test", 1000, 12, 10, false));
 
-        // симуляція введення
         String input = "1\n1000\n"; // id=1, погашення 1000
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        // виконання команди
+
         EarlyPayoffCommand cmd = new EarlyPayoffCommand(system);
         cmd.execute();
 
-        // отримуємо кредит
+
         var credit = system.getCreditById(1);
 
-        // ✔ Кредит не видалений
+
         Assertions.assertNotNull(credit, "Кредит НЕ має бути видалений");
 
-        // ✔ Сума стала 0
         Assertions.assertEquals(0.0, credit.getSum(), 0.0001,
                 "Сума після повного погашення має бути 0");
 
-        // ✔ Статус CLOSED
+
         Assertions.assertEquals(CreditStatus.CLOSED, credit.getStatus(),
                 "Після повного погашення кредит має бути CLOSED");
     }
